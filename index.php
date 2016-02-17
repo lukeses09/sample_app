e95259b3c6b7bb879b2d080a9500ea6d72067484
Lukeses <lukeses09@gmail.com>
2016-02-18 01:45:36 +0800

    Separated populate method as controller js at serverside" "

--- index.php
+++ index.php
@@ -62,31 +62,10 @@
 
 </body>
 </html>
+<script src="serverside/controller.js" type="text/javascript"></script>
 
 
 <script> /* CONTROLLER SCRIPT */
-
-  function populate_mytable(){ 
-    //ajax now
-    $.ajax ({
-      type: "POST",
-      url: "serverside/populate_mytable.php",
-      dataType: 'json',      
-      cache: false,
-      success: function(x)
-      {
-        mytable.fnClearTable();        
-        for(var i = 0; i < x.length; i++) 
-        { 
-          mytable.fnAddData
-          ([
-						x[i][0],x[i][1],x[i][2],     
-          ]); 
-        }       
-      }  
-    }); 
-    //ajax end  
-  } //.load cat_table
 
 
 </script> 
